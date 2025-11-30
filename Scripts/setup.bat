a24c4ca3ba7df3594eb81c01b1edfa2b99c11d8a
zLouis043 <louis.003.lt@gmail.com>
2025-11-30 16:41:41 +0100

    hopefully fixed ci/cd

--- Scripts/setup.bat
+++ Scripts/setup.bat
@@ -1,6 +1,6 @@
 cd ../vendor/vcpkg/ 
-call ./bootstrap-vcpkg.bat
-call ./vcpkg install lua
-call ./vcpkg install sol2
-call ./vcpkg install spdlog
-call ./vcpkg install doctest
+./bootstrap-vcpkg.bat
+vcpkg install lua
+vcpkg install sol2
+vcpkg install spdlog
+vcpkg install doctest
